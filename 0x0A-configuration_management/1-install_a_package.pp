0701d8b66e54726e2cd8b8a6901fc2db837332fd
Eric Karanja <erickaranja54@gmail.com>
2023-11-27 19:07:35 +0300

    config mgt

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Generate an RSA key pair with a 4096-bit key length and a passphrase
-ssh-keygen -b 4096 -f school -t rsa -P hello
+ssh-keygen -b 4096 -f school -t rsa -P school 
