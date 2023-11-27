7e5a4c4dab78382817353dad09b995aee0580b35
Eric Karanja <erickaranja54@gmail.com>
2023-11-27 19:02:08 +0300

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
-# Installs puppet-lint
-package { 'puppet-lint':
-ensure   => '2.5.0',
-provider => gem,
-}
+#!/usr/bin/env bash
+# Generate an RSA key pair with a 4096-bit key length and a passphrase
+ssh-keygen -f ~/.ssh/school -b 4096 -N "betty"
+echo "RSA key pair created successfully!"
